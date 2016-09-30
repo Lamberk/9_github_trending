d8c98261da44ce62f1ae231c517d6235536bf6eb
Komarov Valentin <komarovvv36@gmail.com>
2016-09-30 18:44:06 +0300

    First commit

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,44 @@
+import requests
+import json
+import datetime
+
+
+API_GITHUB_URL = 'https://api.github.com/'
+
+
+DAYS_NUMBER = 7
+
+
+TOP_SIZE = 20
+
+
+def get_date_before_today(days_number):
+    return datetime.date.today() - datetime.timedelta(days_number)
 
 
 def get_trending_repositories(top_size):
-    pass
+    date = get_date_before_today(DAYS_NUMBER)
+    url = '{}search/repositories?q=created:>{}&sort=stars'.format(
+        API_GITHUB_URL, date
+    )
+    data = json.loads(requests.get(url).text)
+    return data['items'][:top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    url = '{}repos/{}/{}/issues?state=open'.format(
+        API_GITHUB_URL, repo_owner, repo_name
+    )
+    return json.loads(requests.get(url).text)
+
 
 if __name__ == '__main__':
-    pass
+    data = get_trending_repositories(TOP_SIZE)
+    for repo in data:
+        issue_data = get_open_issues_amount(
+            repo['owner']['login'], repo['name']
+        )
+        print('#'*80)
+        print('Repository name:', repo['name'])
+        print('Repository url:', repo['html_url'])
+        print('Number of open issues:', len(issue_data))
